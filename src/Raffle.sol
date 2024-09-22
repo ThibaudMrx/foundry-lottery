2260729877047781720f2fc2ebf7a7395ca0dcf0
thibaudmrx <thibaudmerieux@gmail.com>
2024-09-22 20:50:37 +0200

    Commit Premier soir

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,5 +1,21 @@
-//SPDX-License-Identifier: SEE LICENSE IN LECENSE
+//SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
 
-contract Raffle {}
+/**
+ * @title A Simple Raffle contract
+ * @author Thibaud Merieux
+ * @notice This contract is for creating a simple Raffle
+ * @dev Implements Chainlink VRF2.5
+ */
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle(uint256 entranceFee) public {}
+
+    function pickWinner() public {}
+}
